f2bd6d00f40dca066fee233f3c3a143e16ebd6fa
Mark Soule <mark.soule@improving.com>
2025-06-12 19:17:20 -0500

    Pulled in swc/core - testing that it will do anything at all

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,24 @@
 import * as core from "@actions/core";
+import { parseSync } from "@swc/core";
 
 async function main() {
 	const modelVersion = core.getInput("MODEL_VERSION");
 
 	console.log(`The model version is: ${modelVersion}`);
+
+	const sourceCode = `import * as core from "@actions/core";
+async function main() {
+	const modelVersion = core.getInput("MODEL_VERSION");
+}
+main();`;
+
+	const ast = parseSync(sourceCode, {
+		syntax: 'typescript',
+		tsx: false,
+	});
+
+	console.log("--------- HERE IS YOUR AST --------------")
+	console.log(JSON.stringify(ast));
 }
 
 main();
